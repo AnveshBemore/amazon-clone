f0427d4fe539627346d06e914e0b401d1889b10c
AnveshBemore <anveshreddy.bemore@gmail.com>
2020-10-22 20:56:14 +0530

    amazon clone search bar

---
+++ src/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img  className="header__logo"
+            src="https://www.shutterstock.com/image-photo/kiev-ukraine-may-07-2015-amazon-276929006"/>
+            <div
+            className="header__search">        
+            <input
+            className="header__searchInput" type="text"/>
+            <SearchIcon
+            className="header__searchIcon"/>
+            </div>
+            
+           <div className="header__nav">
+                 <div className='header__option'>
+                     <span className='header__optionLineOne'>
+                         Hello Guest
+                     </span>
+                     <span className='header__optionLineTwo'>
+                         Sign In
+                     </span>
+                 </div>
+
+                 <div className='header__option'>
+                 <span className='header__optionLineOne'>
+                         Returns
+                     </span>
+                     <span className='header__optionLineTwo'>
+                         &Orders
+                     </span>
+                 </div>
+
+                 <div className='header__option'>
+                 <span className='header__optionLineOne'>
+                         Your
+                     </span>
+                     <span className='header__optionLineTwo'>
+                         prime
+                     </span>
+
+                
+                 </div>
+            </div>
+
+
+            </div> 
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBPmtd8h6dpttSFPYdhF3JOxXZEr1UQLcw",
+    authDomain: "farmers-produce.firebaseapp.com",
+    databaseURL: "https://farmers-produce.firebaseio.com",
+    projectId: "farmers-produce",
+    storageBucket: "farmers-produce.appspot.com",
+    messagingSenderId: "765414416706",
+    appId: "1:765414416706:web:55be3669d4155ff5ebe8db",
+    measurementId: "G-J1DQVNJ29Z"
+  };
